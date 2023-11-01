545f72d33113e49c8171d485f9d3ef2c2e8a18e7
Moshe Yosef Cohen <mycohen1234@gmail.com>
2023-11-01 10:38:27 +0200

    added MUI

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import UserNotifications from './Components/UserNotifications';
 import SearchInput from './Components/SearchInput';
 import Header from './Components/Header';
+import { Button } from '@mui/material';
 function App() {
   
 
   return (
     <div>
-     <Header/>      
+      
+     <Header/>  
+     <Button variant='contained' color='error'>hello</Button>    
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CssBaseline from '@mui/material/CssBaseline';
+import App from "./App.jsx";
+import "./index.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#F6C927",
+    },
+    secondary: {
+      main: "#0A0A1B",
+    },
+    background: {
+      main: "#21213E",
+    },
+
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
