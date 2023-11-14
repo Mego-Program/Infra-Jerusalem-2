1b2bed0b51a2d818b232c3e5a12739037056667e
eli barak <barak3656@gmail.com>
2023-11-14 15:17:18 +0200

    Committing changes before merging main

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,27 @@ import SignIn from "./Components/SignIn";
 import Header from "./Components/Header";
 import SignUp from "./Components/SignUp";
 
+import axios from "axios";
+import { Token } from "@mui/icons-material";
+
+const askServer = async (reqName) =>{
+  try {
+  const token = localStorage.getItem('token')
+  
+    const response = await axios.get('http://localhost:5173/' + reqName, token);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+
+  }
+
+}
+
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" >
-        <Route path="signin" element={<SignIn />}></Route>
+        <Route path="signin" element={<SignIn />} ></Route>
         <Route path="signup" element={<SignUp />}></Route>
         <Route path="dashboard" element={<Header />}></Route>
         <Route index element={<SignUp />}></Route>
