2640a31a9511e881d8624d67b07ba04a2721ce0e
nguyaniv <nguyaniv@gmail.com>
2023-11-14 12:18:55 +0200

    remove comment - StyledBadge

--- src/Components/BadgeAvatars.jsx
+++ src/Components/BadgeAvatars.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
-import { styled } from '@mui/material/styles';
-import Badge from '@mui/material/Badge';
-import Avatar from '@mui/material/Avatar';
-import { Button } from '@mui/material';
+import React, { useState } from "react";
+import { styled } from "@mui/material/styles";
+import Badge from "@mui/material/Badge";
+import Avatar from "@mui/material/Avatar";
+import { Button } from "@mui/material";
 
-// Styled Badge component with custom styling
 const StyledBadge = styled(Badge)(({ theme }) => ({
-  '& .MuiBadge-badge': {
-    backgroundColor: '#44b700',
-    color: '#44b700',
+  "& .MuiBadge-badge": {
+    backgroundColor: "#44b700",
+    color: "#44b700",
   },
 }));
 
@@ -25,14 +24,11 @@ export default function BadgeAvatars() {
   };
 
   return (
-    <Button 
-      sx={{ marginRight: '2vh' }} 
-      onClick={handleBadgeVisibility}
-    >
+    <Button sx={{ marginRight: "2vh" }} onClick={handleBadgeVisibility}>
       {/* Avatar with an overlaid badge */}
       <StyledBadge
         overlap="circular"
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
         variant="dot"
         invisible={invisible}
       >
