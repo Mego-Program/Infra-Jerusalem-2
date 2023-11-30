0cbd497fcf4a98a274f546770f0e98c0692a7590
Refael Perets <Refael1peretz@gmail.com>
2023-11-30 15:32:25 +0200

    In AuthLayout: add a logo, and about the site, and design the buttons.

--- src/pages/connection/SignIn.jsx
+++ src/pages/connection/SignIn.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import axios from 'axios';
-import { NavLink, useNavigate } from 'react-router-dom';
-import useCameFromNavigation from '../../atom/userAtom.js'
+import React, { useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import axios from "axios";
+import { NavLink, useNavigate } from "react-router-dom";
+import useCameFromNavigation from "../../atom/userAtom.js";
 const theme = createTheme();
 
 const textFieldStyles = {
@@ -36,13 +36,12 @@ const textFieldStyles = {
   "& .MuiOutlinedInput-input.Mui-focused": {
     color: "white",
   },
-  "&:hover, &:hover .MuiInputLabel-root, &:hover .MuiOutlinedInput-notchedOutline": {
-    borderColor: "#F6C927",
-    color: "#F6C927",
-  },
+  "&:hover, &:hover .MuiInputLabel-root, &:hover .MuiOutlinedInput-notchedOutline":
+    {
+      borderColor: "#F6C927",
+      color: "#F6C927",
+    },
 };
-
-
 
 const buttonStyles = {
   mt: 3,
@@ -52,17 +51,16 @@ const buttonStyles = {
   "&:hover": {
     backgroundColor: "#21213E",
     borderColor: "#F6C927",
-
   },
   "&.Mui-focusVisible": {
     backgroundColor: "#21213E",
     borderColor: "#F6C927",
   },
-  "& input:-webkit-autofill, & input:-webkit-autofill:hover, & input:-webkit-autofill:focus, & input:-webkit-autofill:active": {
-    WebkitBoxShadow: "0 0 0 30px #121231 inset !important",
-    WebkitTextFillColor: "white !important",
-  },
-  
+  "& input:-webkit-autofill, & input:-webkit-autofill:hover, & input:-webkit-autofill:focus, & input:-webkit-autofill:active":
+    {
+      WebkitBoxShadow: "0 0 0 30px #121231 inset !important",
+      WebkitTextFillColor: "white !important",
+    },
 };
 
 const checkboxStyles = {
@@ -91,22 +89,22 @@ export default function SignIn() {
         email: data.get("email"),
         password: data.get("password"),
       });
-      if (response.status === 200){
+      if (response.status === 200) {
         console.log(response);
         setToken(response.data.token);
-      localStorage.setItem("token", response.data.token);
-      
-      navigateRootLayout('/rootLayout')
+        localStorage.setItem("token", response.data.token);
 
+        navigateRootLayout("/rootLayout");
       }
-      
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <Box sx={{ p: "50px", bgcolor: "#121231", minHeight: "100vh", color: "white" }}>
+    <Box
+      sx={{ p: "50px", bgcolor: "#121231", minHeight: "100vh", color: "white" }}
+    >
       <ThemeProvider theme={theme}>
         <Container component="main" maxWidth="xs">
           <CssBaseline />
@@ -118,11 +116,32 @@ export default function SignIn() {
               alignItems: "center",
             }}
           >
-            <Avatar
-              sx={{ m: 3, bgcolor: "#21213E", width: "50px", height: "50px" }}
+            <svg
+              width="50px"
+              height="50px"
+              viewBox="0 0 87 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <LockOutlinedIcon sx={{ color: "#F6C927" }} />
-            </Avatar>
+              <path
+                d="M44.2397 2.69504L43.5 2.27581L42.7603 2.69504L22.5328 14.16L22.5225 14.1659L22.5122 14.1719L2.46952 25.957L1.73663 26.3879L1.72987 27.2381L1.54505 50.4881L1.54495 50.5L1.54505 50.5119L1.72987 73.7619L1.73663 74.6121L2.46952 75.043L22.5122 86.8281L22.5225 86.8341L22.5328 86.84L42.7603 98.305L43.5 98.7242L44.2397 98.305L64.4671 86.84L64.4775 86.8341L64.4878 86.8281L84.5305 75.043L85.2634 74.6121L85.2701 73.7619L85.455 50.5119L85.455 50.5L85.455 50.4881L85.2701 27.2381L85.2634 26.3879L84.5305 25.957L64.4878 14.1719L64.4775 14.1659L64.4671 14.16L44.2397 2.69504Z"
+                stroke="#F6C927"
+                stroke-width="3"
+              />
+              <path
+                d="M36.2897 60.414L22 54.0032V48.4682L36.2897 42.0573V47.3471L23.6422 52.742V49.7293L36.2897 55.1242V60.414Z"
+                fill="#F6C927"
+              />
+              <path
+                d="M36.8754 67L46.3827 34H51.3956L41.8883 67H36.8754Z"
+                fill="#F6C927"
+              />
+              <path
+                d="M51.7103 60.414V55.1242L64.3578 49.7293V52.742L51.7103 47.3471V42.0573L66 48.4682V54.0032L51.7103 60.414Z"
+                fill="#F6C927"
+              />
+            </svg>
+
             <Typography component="h1" variant="h5">
               Sign In
             </Typography>
@@ -133,7 +152,6 @@ export default function SignIn() {
               sx={{ mt: 1 }}
             >
               <TextField
-              
                 margin="normal"
                 required
                 fullWidth
@@ -156,7 +174,7 @@ export default function SignIn() {
                 sx={textFieldStyles}
               />
               <FormControlLabel
-                control={<Checkbox value="remember"  sx={checkboxStyles} />}
+                control={<Checkbox value="remember" sx={checkboxStyles} />}
                 label={
                   <Typography variant="body2" sx={rememberMeStyles}>
                     Remember me
@@ -173,7 +191,11 @@ export default function SignIn() {
               </Button>
               <Grid container>
                 <Grid item xs>
-                  <NavLink href="#" variant="body2" style={{ color: "#F6C927" }}>
+                  <NavLink
+                    href="#"
+                    variant="body2"
+                    style={{ color: "#F6C927" }}
+                  >
                     Forgot password?
                   </NavLink>
                 </Grid>
