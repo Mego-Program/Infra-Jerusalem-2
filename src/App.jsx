61172cd239b90b9874287024631977c6f27b4802
MosheBrim <brim0533167854@gmail.com>
2023-12-05 15:45:32 +0200

    Correcting routing in the micro frontend and connecting all the projects successfully

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-// Importing pages
 import Dashboard from "./pages/mainMenu/Dashboard";
-import Projects from "./pages/mainMenu/Projects";// delete
-import Board from "./pages/mainMenu/Board";
 import AddUser from "./pages/mainMenu/AddUser";
 import Messages from "./pages/mainMenu/Messages";
 import Settings from "./pages/mainMenu/Settings";
@@ -21,44 +18,33 @@ import RootLayout from "./layouts/RootLayout";
 import AuthLayout from "./layouts/AuthLayout";
 import useUserDetails from "./atom/userAtom";
 import axios from "axios";
-import  Refresh  from "./pages/connection/Refresh";
+import Refresh from "./pages/connection/Refresh";
 
-import MainProjects  from "project/AppProjects"; // cahnge to appProject
-// import AppCommunication from "communication/AppCommunication";
-import SpecsApp from "../../Jlm-Specs-2-main/src/SpecsApp"
-
-
+import MainProjects from "project/AppProjects";
+import AppCommunication from "communication/AppCommunication";
+import SpecsApp from "specs/SpecsApp";
 
 const defaultRouter = createBrowserRouter(
   createRoutesFromElements(<Route path="*" element={<Refresh />} />)
 );
-// Creating a router using react-router-dom
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/">
-      {/* Routes for authentication */}
       <Route index element={<AuthLayout />} />
       <Route path="sign-in" element={<SignIn />} />
       <Route path="sign-up" element={<SignUp />} />
 
-      {/* Routes for the main application with RootLayout */}
-
       <Route path="root-layout" element={<RootLayout />}>
-        {/* Default route for the main application */}
         <Route index element={<Dashboard />} />
-
-        {/* Individual routes for each main menu item */}
         <Route path="dashboard" element={<Dashboard />} />
-        <Route path="projects" element={<MainProjects/>} />
-        <Route path="board" element={<SpecsApp />} />
+        <Route path="projects" element={<MainProjects />} />
+        <Route path="board">{SpecsApp}</Route>
         <Route path="add-user" element={<AddUser />} />
-        <Route path="messages" element={<Messages />} />
+        <Route path="messages" element={<AppCommunication />} />
         <Route path="settings" element={<Settings />} />
         <Route path="info" element={<Info />} />
       </Route>
 
-
-      {/* 404 Not Found route */}
       <Route path="*" element={<NotFound />} />
     </Route>
   )
@@ -67,87 +53,66 @@ const router = createBrowserRouter(
 const invalidRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/">
-      {/* Routes for authentication */}
       <Route index element={<AuthLayout />} />
       <Route path="sign-in" element={<SignIn />} />
       <Route path="sign-up" element={<SignUp />} />
 
-      {/* Routes for the main application with RootLayout */}
-
       <Route path="root-layout" element={<RootLayout />}>
-        {/* Default route for the main application */}
         <Route index element={<Dashboard />} />
 
-        {/* Individual routes for each main menu item */}
         <Route path="dashboard" element={<SignIn />} />
-        <Route path="projects" element={<MainProjects />} />
-        <Route path="board" element={<SpecsApp />} />
+        <Route path="projects" element={<SignIn />} />
+        <Route path="board" element={<SignIn />} />
         <Route path="add-user" element={<SignIn />} />
         <Route path="messages" element={<SignIn />} />
         <Route path="settings" element={<SignIn />} />
         <Route path="info" element={<SignIn />} />
       </Route>
 
-      {/* 404 Not Found route */}
       <Route path="*" element={<NotFound />} />
     </Route>
   )
 );
 
+async function getUserDetails() {
+  try {
+    const token = localStorage.getItem("token");
 
-async function getUserDetails(){
-  try{
-    const token = localStorage.getItem('token')
-    
-    const response = await axios.get(
-      "http://localhost:3000/userDetails"
-      ,{
-      headers:{
-        Authorization:token
-      }
-
+    const response = await axios.get("http://localhost:3000/userDetails", {
+      headers: {
+        Authorization: token,
+      },
     });
     console.log(response);
 
-
-    if (response.status === 200){
-      
-
-    return response.data}
-    else{
-      console.log('no token');
-
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      console.log("no token");
     }
-  }
-  catch(error){
-    
+  } catch (error) {
     console.log(error);
-
   }
 }
-
 
 // Main App component that provides the router
 function App() {
   const [userDetails, setUserDetails] = useUserDetails();
-  const [token,setTokenProvided] = useState(false)
-  useLayoutEffect(()=>{
-    const fetchUserDetails = async()=>{
+  const [token, setTokenProvided] = useState(false);
+  useLayoutEffect(() => {
+    const fetchUserDetails = async () => {
       const data = await getUserDetails();
-      if(data.status !== 200){
-        setTokenProvided(false)
+      if (data.status !== 200) {
+        setTokenProvided(false);
       }
       console.log(data);
 
-      setTokenProvided(true)
+      setTokenProvided(true);
 
-
-      setUserDetails(data)
-      
-
-    }
-    fetchUserDetails()
-  },[])
+      setUserDetails(data);
+    };
+    fetchUserDetails();
+  }, []);
 
   return (
     <RouterProvider
