1dc557c9466d338e5450a770c0d8f7e927baee1d
Refael Perets <Refael1peretz@gmail.com>
2024-01-04 16:54:56 +0200

    added editShortProfile

--- src/Components/MyProfile.jsx
+++ src/Components/MyProfile.jsx
@@ -1,46 +1,47 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
-import DialogTitle from "@mui/material/DialogTitle";
 import Dialog from "@mui/material/Dialog";
 import { useState, useEffect, useRef } from "react";
 import axios from "axios";
-import { color } from "@mui/system";
-import { Typography } from "@mui/material";
-
-//  validation
-const validationSchema = Yup.object({
-  firstName: Yup.string().required("Required"),
-  lastName: Yup.string().required("Required"),
-  newPassword: Yup.string()
-    .min(8, "Password must be at least 8 characters")
-    .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/,
-      "Password must contain at least one uppercase letter, one lowercase letter, and one number"
-    )
-    .required("Required"),
-
-  verifyPassword: Yup.string()
-    .oneOf([Yup.ref("newPassword"), null], "Passwords must match")
-    .required("Required"),
-});
+import { Avatar, Typography } from "@mui/material";
 
 const MyProfile = (props) => {
+//=====================================================
+const [isButtonOn, setIsButtonOn] = useState(false);
+const toggleButton = () => {
+  setIsButtonOn(!isButtonOn);
+};
+//======================================================
+
+  //  validation
+  const validationSchema = Yup.object({
+    firstName: Yup.string().required("Required"),
+    lastName: Yup.string().required("Required"),
+    ...(isButtonOn && {
+      newPassword: Yup.string()
+        .min(8, "Password must be at least 8 characters")
+        .matches(
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/,
+          "Password must contain at least one uppercase letter, one lowercase letter, and one number"
+        )
+        .required("Required"),
+
+      verifyPassword: Yup.string()
+        .oneOf([Yup.ref("newPassword"), null], "Passwords must match")
+        .required("Required"),
+    }),
+  });
+
   const { onClose, selectedValue, open } = props;
 
   const [userDetails, setUserDetails] = useState({});
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  const [newPassword, setnewPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
   const [img, setImg] = useState("");
   const [isHovered, setIsHovered] = useState(false);
 
-  //=====================================================
-  const [isButtonOn, setIsButtonOn] = useState(false);
-  const toggleButton = () => {
-    setIsButtonOn(!isButtonOn);
-  };
-  //======================================================
   const fetchUserDetails = async () => {
     try {
       const token = localStorage.getItem("token");
@@ -72,7 +73,9 @@ const MyProfile = (props) => {
 
   const handleClose = () => {
     onClose(selectedValue);
-    toggleButton();
+    if (isButtonOn) {
+      toggleButton();
+    }
   };
 
   const initialValues = {
@@ -85,37 +88,59 @@ const MyProfile = (props) => {
   const onSubmit = async (values) => {
     console.log("form data:", values);
     const token = localStorage.getItem("token");
-    console.log(token);
-
     setFirstName(values.firstName);
     setLastName(values.lastName);
-    setnewPassword(values.newPassword);
-
-    try {
-      const response = await axios.post(
-        "https://infra-jerusalem-2-server.vercel.app/editProfile",
-        {
-          firstName: firstName,
-          lastName: lastName,
-          userName: userDetails.userName,
-          password: newPassword,
-          email: userDetails.email,
-          img: img,
-        },
-        {
-          headers: {
-            authorization: token,
+    if (isButtonOn) {
+      try {
+        const response = await axios.post(
+          "https://infra-jerusalem-2-server.vercel.app/editProfile",
+          {
+            firstName: values.firstName,
+            lastName: values.lastName,
+            userName: userDetails.userName,
+            password: values.newPassword,
+            email: userDetails.email,
+            img: img,
           },
-        }
-        
-      );
+          {
+            headers: {
+              authorization: token,
+            },
+          }
+        );
 
-      if (response.status === 200) {
-        console.log(response);
-        handleClose();
+        if (response.status === 200) {
+          console.log(response);
+          handleClose();
+        }
+      } catch (error) {
+        console.error(error);
       }
-    } catch (error) {
-      console.error(error);
+    } else {
+      try {
+        const response = await axios.post(
+          "https://infra-jerusalem-2-server.vercel.app/editShortProfile",
+          {
+            firstName: values.firstName,
+            lastName: values.lastName,
+            userName: userDetails.userName,
+            email: userDetails.email,
+            img: img,
+          },
+          {
+            headers: {
+              authorization: token,
+            },
+          }
+        );
+
+        if (response.status === 200) {
+          console.log(response);
+          handleClose();
+        }
+      } catch (error) {
+        console.error(error);
+      }
     }
   };
 
@@ -193,52 +218,99 @@ const MyProfile = (props) => {
                 justifyContent: "space-between",
               }}
             >
-              <div
-                style={{
-                  width: "100px",
-                  height: "100px",
-                  position: "relative",
-                }}
-              >
-                <img
-                  src={img}
-                  alt=""
+              {img ? (
+                <div
                   style={{
                     width: "100px",
                     height: "100px",
-                    borderRadius: "50%",
+                    position: "relative",
                   }}
-                />
-                <button
-                  style={{
-                    position: "absolute",
-                    width: "20px",
-                    height: "20px",
-                    bottom: "8px",
-                    right: "-5px",
-                    backgroundColor: "transparent",
-                    border: "0",
-                  }}
-                  onClick={() => widgetRef.current.open()}
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 576 512"
+                  <img
+                    src={img}
+                    alt=""
+                    style={{
+                      width: "100px",
+                      height: "100px",
+                      borderRadius: "50%",
+                    }}
+                  />
+                  <button
                     style={{
                       position: "absolute",
-                      bottom: "-0.5px",
-                      right: "-3.5px",
-                      width: "22px",
-                      height: "22px",
-                      fill: isHovered ? "rgb(249, 227, 149)" : "#f6c927",
+                      width: "20px",
+                      height: "20px",
+                      bottom: "8px",
+                      right: "-5px",
+                      backgroundColor: "transparent",
+                      border: "0",
                     }}
-                    onMouseEnter={() => setIsHovered(true)}
-                    onMouseLeave={() => setIsHovered(false)}
+                    onClick={() => widgetRef.current.open()}
                   >
-                    <path d="M402.6 83.2l90.2 90.2c3.8 3.8 3.8 10 0 13.8L274.4 405.6l-92.8 10.3c-12.4 1.4-22.9-9.1-21.5-21.5l10.3-92.8L388.8 83.2c3.8-3.8 10-3.8 13.8 0zm162-22.9l-48.8-48.8c-15.2-15.2-39.9-15.2-55.2 0l-35.4 35.4c-3.8 3.8-3.8 10 0 13.8l90.2 90.2c3.8 3.8 10 3.8 13.8 0l35.4-35.4c15.2-15.3 15.2-40 0-55.2zM384 346.2V448H64V128h229.8c3.2 0 6.2-1.3 8.5-3.5l40-40c7.6-7.6 2.2-20.5-8.5-20.5H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V306.2c0-10.7-12.9-16-20.5-8.5l-40 40c-2.2 2.3-3.5 5.3-3.5 8.5z" />
-                  </svg>
-                </button>
-              </div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 576 512"
+                      style={{
+                        position: "absolute",
+                        bottom: "-0.5px",
+                        right: "-3.5px",
+                        width: "22px",
+                        height: "22px",
+                        fill: isHovered ? "rgb(249, 227, 149)" : "#f6c927",
+                      }}
+                      onMouseEnter={() => setIsHovered(true)}
+                      onMouseLeave={() => setIsHovered(false)}
+                    >
+                      <path d="M402.6 83.2l90.2 90.2c3.8 3.8 3.8 10 0 13.8L274.4 405.6l-92.8 10.3c-12.4 1.4-22.9-9.1-21.5-21.5l10.3-92.8L388.8 83.2c3.8-3.8 10-3.8 13.8 0zm162-22.9l-48.8-48.8c-15.2-15.2-39.9-15.2-55.2 0l-35.4 35.4c-3.8 3.8-3.8 10 0 13.8l90.2 90.2c3.8 3.8 10 3.8 13.8 0l35.4-35.4c15.2-15.3 15.2-40 0-55.2zM384 346.2V448H64V128h229.8c3.2 0 6.2-1.3 8.5-3.5l40-40c7.6-7.6 2.2-20.5-8.5-20.5H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V306.2c0-10.7-12.9-16-20.5-8.5l-40 40c-2.2 2.3-3.5 5.3-3.5 8.5z" />
+                    </svg>
+                  </button>
+                </div>
+              ) : (
+                <div
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                    position: "relative",
+                  }}
+                >
+                  <Avatar
+                    style={{
+                      width: "100px",
+                      height: "100px",
+                      borderRadius: "50%",
+                    }}
+                  />
+                  <button
+                    style={{
+                      position: "absolute",
+                      width: "20px",
+                      height: "20px",
+                      bottom: "8px",
+                      right: "-5px",
+                      backgroundColor: "transparent",
+                      border: "0",
+                    }}
+                    onClick={() => widgetRef.current.open()}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 576 512"
+                      style={{
+                        position: "absolute",
+                        bottom: "-0.5px",
+                        right: "-3.5px",
+                        width: "22px",
+                        height: "22px",
+                        fill: isHovered ? "rgb(249, 227, 149)" : "#f6c927",
+                      }}
+                      onMouseEnter={() => setIsHovered(true)}
+                      onMouseLeave={() => setIsHovered(false)}
+                    >
+                      <path d="M402.6 83.2l90.2 90.2c3.8 3.8 3.8 10 0 13.8L274.4 405.6l-92.8 10.3c-12.4 1.4-22.9-9.1-21.5-21.5l10.3-92.8L388.8 83.2c3.8-3.8 10-3.8 13.8 0zm162-22.9l-48.8-48.8c-15.2-15.2-39.9-15.2-55.2 0l-35.4 35.4c-3.8 3.8-3.8 10 0 13.8l90.2 90.2c3.8 3.8 10 3.8 13.8 0l35.4-35.4c15.2-15.3 15.2-40 0-55.2zM384 346.2V448H64V128h229.8c3.2 0 6.2-1.3 8.5-3.5l40-40c7.6-7.6 2.2-20.5-8.5-20.5H48C21.5 64 0 85.5 0 112v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V306.2c0-10.7-12.9-16-20.5-8.5l-40 40c-2.2 2.3-3.5 5.3-3.5 8.5z" />
+                    </svg>
+                  </button>
+                </div>
+              )}
               <div
                 style={{
                   display: "flex",
